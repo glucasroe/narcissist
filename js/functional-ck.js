9701e9c4eaec50ba61c060a14cd992cbe5637f0c
glucasroe <glucasroe@gmail.com>
2014-01-29 00:32:50 -0500

    HAH, FINALLY GOT THE ANIMATION WORKING. In other news, I really need to read up and understand CSS animations.

--- js/functional-ck.js
+++ js/functional-ck.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("nav").hide();$("#nav_toggle").removeClass();$("#nav_toggle").click(function(){$("#nav_toggle i").toggleClass("rotated")})});
+$(document).ready(function(){$("nav").hide();$("#nav_toggle").removeClass();$("#nav_toggle").click(function(){$("#nav_toggle i").toggleClass("rotated");$("nav").slideToggle(200)})});
--- js/functional.js
+++ js/functional.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	$('#nav_toggle').removeClass();
 	$('#nav_toggle').click(function(){
 		$('#nav_toggle i').toggleClass('rotated');
+		$('nav').slideToggle(200);
 	});
 
 });
