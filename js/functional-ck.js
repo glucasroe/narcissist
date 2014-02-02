7b70ffe512d9af8f38313f312fd6abeedebd8aa4
glucasroe <glucasroe@gmail.com>
2014-02-01 22:49:33 -0500

    no-js class to hide toggle if jQuery fails

--- js/functional-ck.js
+++ js/functional-ck.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("nav").addClass("shrink_hider");$("#nav_toggle").click(function(){$("nav:not(.animated)").addClass("animated");$("#nav_toggle i").toggleClass("rotated");$("nav").toggleClass("shrink_hider")})});
+$(document).ready(function(){$("nav").addClass("shrink_hider");$("#nav_toggle").removeClass("no-js").click(function(){$("nav:not(.animated)").addClass("animated");$("#nav_toggle i").toggleClass("rotated");$("nav").toggleClass("shrink_hider")})});
--- js/functional.js
+++ js/functional.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('nav').addClass('shrink_hider');
-	$('#nav_toggle').click(function(){
+	$('#nav_toggle').removeClass('no-js').click(function(){
 		$('nav:not(.animated)').addClass('animated');
 		$('#nav_toggle i').toggleClass('rotated');
 		$('nav').toggleClass('shrink_hider');
