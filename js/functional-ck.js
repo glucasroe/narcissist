6d78d8ca7f2a39a6d6cea8e1f74f39cfcd087a4b
glucasroe <glucasroe@gmail.com>
2014-03-16 18:23:43 -0400

    Moved menu above header, added menu text, time to get responsive.

--- js/functional-ck.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$("body").css("opacity","0").delay(300).animate({opacity:"1"},200);$("nav").addClass("shrink_hider");$("#nav_toggle").removeClass("no-js").click(function(){$("nav:not(.animated)").addClass("animated");$("#nav_toggle i").toggleClass("rotated");$("nav").toggleClass("shrink_hider")})});
--- precompiled/functional.js
+++ precompiled/functional.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$('body').css('opacity','0').delay(300).animate({"opacity": "1"}, 200);
+	$('body').css('opacity','0').delay(100).animate({"opacity": "1"}, 300);
 	$('nav').addClass('shrink_hider');
 	$('#nav_toggle').removeClass('no-js').click(function(){
 		$('nav:not(.animated)').addClass('animated');
