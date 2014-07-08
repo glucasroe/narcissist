21d69450598c47d8209d1ac908202e35f45d6186
glucasroe <glucasroe@gmail.com>
2014-07-07 22:24:32 -0400

    About to start templating, omg

--- precompiled/functional.js
+++ precompiled/functional.js
@@ -4,10 +4,13 @@ $(document).ready(function(){
 	$('nav').addClass('shrink_hider');
 	$('#nav_toggle').removeClass('no-js').click(function(){
 		$('nav:not(.animated)').addClass('animated');
-		$('#nav_toggle i').toggleClass('rotated');
+		$('#nav_toggle i').not('#search_icon').toggleClass('rotated');
 		$('nav').toggleClass('shrink_hider');
 	}).children().not('i,p').click(function(e){
 		return false;
+	});
+	$('#search_icon').click(function(){
+		$('#search_field').focus();
 	});
 });
 
