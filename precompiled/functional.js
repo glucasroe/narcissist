2ce99553c7b8032a044a35e207355a66fb93a3e1
glucasroe <glucasroe@gmail.com>
2014-10-13 23:56:55 -0400

    HOLY BUGFIXES BATMAN. GfyCat doesn't look like ass anymore, fixed inline images, converted everything to classes instead of IDs. Unnesting to come in the future.

--- precompiled/functional.js
+++ precompiled/functional.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
 
 	$('body').css('opacity','0').delay(100).animate({"opacity": "1"}, 300);
 	$('nav').not('.pagination').addClass('shrink_hider');
-	$('#nav_toggle').removeClass('no-js').click(function(){
+	$('.nav_toggle').removeClass('no-js').click(function(){
 		$('nav:not(.animated)').addClass('animated');
-		$('#nav_toggle i').not('#search_icon').toggleClass('rotated');
+		$('.nav_toggle i').not('.search_icon').toggleClass('rotated');
 		$('nav').toggleClass('shrink_hider');
 	}).children().not('i,p').click(function(e){
 		return false;
 	});
-	$('#search_icon').click(function(){
-		$('#search_field').focus();
+	$('.search_icon').click(function(){
+		$('.search_field').focus();
 	});
 	var $eles = $('p').filter(function() {
     	return $(this).text().length == 0;
