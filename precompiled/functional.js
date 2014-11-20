fc893eb5050c589bab2d9e902f64f65bdff1fa6d
glucasroe <glucasroe@gmail.com>
2014-11-19 22:11:18 -0500

    Changing js targeting to apply images-fix

--- precompiled/functional.js
+++ precompiled/functional.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
 	$('.search-icon').click(function(){
 		$('.search-field').focus();
 	});
-	var $eles = $('p').filter(function() {
-    	return $(this).text().length == 0;
-	});	
-	$eles.has('img').addClass("images-fix");
-
+	//var $eles = $('p').filter(function() {
+    //	return $(this).text().length == 0;
+	//});	
+	//$eles.has('img').addClass("images-fix");
+	$('img.big-image').parent('p').addClass('images-fix');
 });
 
