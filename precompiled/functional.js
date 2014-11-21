e883a100a12cd2d0709d51dca239c7567598329e
glucasroe <glucasroe@gmail.com>
2014-11-20 21:00:46 -0500

    Javascript fiddling with tryint to get a one page scroller

--- precompiled/functional.js
+++ precompiled/functional.js
@@ -7,5 +7,18 @@ $(document).ready(function(){
 	//});	
 	//$eles.has('img').addClass("images-fix");
 	$('img.big-image').parent('p').addClass('images-fix');
-});
 
+	var page = 2;
+    var url_blog = window.location;
+    $(window).scroll(function() {
+   		if($(window).scrollTop() + $(window).height() === $(document).height()) {
+			$.get((url_blog +'/page/'+page),
+			function(content) {
+        		if(page <= max_pages){
+        			$('.content').append($(content).children(".post"));
+        			page = page + 1;
+    			}
+			});
+   		}
+	});
+});
